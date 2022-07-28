26882c635bfe592b366fefa316a3355e1eae20fb
Marcus <marcusadrenaline@gmail.com>
2022-07-28 11:02:14 -0300

    ---

---
+++ pratica-sql.sql
@@ -0,0 +1,19 @@
+SELECT COUNT(e."endDate") as "currentExperiences"
+FROM experiences e;
+
+SELECT "userId" as "id", COUNT(id) as "educations"
+FROM educations
+GROUP BY "userId"
+ORDER BY "educations" DESC;
+
+SELECT u.name as "writer", COUNT(t.id) as "testimonailCount"
+FROM testimonials t
+JOIN users u ON
+t."writerId" = u.id
+GROUP BY u.name;
+
+SELECT MAX(j.salary) as "maximumSalary",  r.name as role
+FROM jobs j
+JOIN roles r ON
+r.id = j."roleId"
+GROUP BY role;
